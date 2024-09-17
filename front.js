060fe0a8a1b3ca87f36e53a20e3c9e40b1c452a1
Dutrasilvathiago <127859397+Dutrasilvathiago@users.noreply.github.com>
2024-09-17 11:29:04 -0300

    Add files via upload

--- front.js
+++ front.js
@@ -1,1 +1,9 @@
+function cadastro(){
+    const nome = document.getElementById('nome').value;
+    const email = document.getElementById('em').value;
+    const password = document.getElementById('senha').value;
 
+    document.getElementById('omen').innerText=nome;
+    document.getElementById('liame').innerHTML= email;
+    document.getElementById('ahnes').innerText= password;
+}
