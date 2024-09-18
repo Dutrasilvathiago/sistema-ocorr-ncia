6e2c1a11e7846050c1ee76e06b351eae99f5f672
MaxwellDeLara <127850040+MaxwellDeLara@users.noreply.github.com>
2024-09-18 08:41:46 -0300

    Add files via upload

--- front.js
+++ front.js
@@ -3,10 +3,4 @@ function cadastro(){
     const email = document.getElementById('em').value;
     const cgm = document.getElementById('cgm').value;
     const password = document.getElementById('senha').value;
-    
-    document.getElementById('omen').innerText=nome;
-    document.getElementById('mgc').innerText=cgm;
-    document.getElementById('liame').innerHTML= email;
-    document.getElementById('ahnes').innerText= password;
-    
 }
