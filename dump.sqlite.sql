5090d0cc96119c6a968b3fac3cd8be0443ca5970
MaxwellDeLara <127850040+MaxwellDeLara@users.noreply.github.com>
2024-10-08 11:29:11 -0300

    Add files via upload

---
+++ dump.sqlite.sql
@@ -0,0 +1,24 @@
+-- TABLE
+CREATE TABLE aluno(
+	id integer PRIMARY KEY AUTOINCREMENT,
+    nome varchar(100) not NULL,
+    cgm varchar(20) unique NOT NULL,
+    endereco_b text NOT NULL,
+    endereco_r text NOT NULL,
+    endereco_n text NOT NULL
+);
+CREATE TABLE funcionario(
+  id integer PRIMARY KEY AUTOINCREMENT,
+  nome varchar(100) NOT NULL,
+  cpf varchar(11) UNIQUE NOT NULL,
+  endereco_b text NOT NULL,
+  endereco_r text NOT NULL,
+  endereco_n text NOT NULL
+  );
+ 
+-- INDEX
+ 
+-- TRIGGER
+ 
+-- VIEW
+ 
